0648edf7a488e9ebaceca93b85093bfd9669a55d
Jason Pitts <juniorjr083@gmail.com>
2018-09-17 18:49:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(drivers, name){
 
-  drivers.filter(function(ele){
+ return drivers.filter(function(ele){
     ele == name ? ele : 'no match';
     
     
